ac0db6459c9302fc4d0761202a506ed507e76dbc
phoetry <anymango@gmail.com>
2011-09-28 01:07:44 +0800

    By Phoetry

---
+++ placeholder.js
@@ -0,0 +1,31 @@
+/**
+  * HTML5 Placeholder jQuery Plugin v1.0
+  * For enabling the html5 attribute 'placeholder' on form elements.
+  * When adding code dynamically, run sth. like this:
+  * $node.find('[placeholder]').placeholder();
+  * Or just this:
+  * $('[placeholder]').placeholder();
+  * @Author: Phoetry (http://phoetry.me)
+  * @Url: http://phoetry.me/archives/jquery-placeholder.html
+  **/
+~function($){
+	$.fn.placeholder=function(){
+		return this.each(function(){
+			if('placeholder'in document.createElement(this.tagName))return;
+			var n=1,t=this,
+				txt=t.getAttribute('placeholder');
+			t.value&&t.value!=txt||(n=0,t.value=txt);
+			$(t).bind({
+				focus:function(){
+					n||(n=1,t.value='');
+				},
+				blur:function(){
+					t.value?n=1:(n=0,t.value=txt);
+				}
+			});
+			t.form&&$(t.form).submit(function(){
+				n||(t.value='');
+			});
+		});
+	};
+}(jQuery);
